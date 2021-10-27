327b3640fdf7adc049b990943918b9c61175e554
Burak <burakylddzz@gmail.com>
2021-10-27 22:48:07 +0300

    ICategoryService Interface Oluşturulması

---
+++ NLayerProject.Core/Services/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using NLayerProject.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerProject.Core.Services
+{
+    interface ICategoryService:IService<Category>
+    {
+        Task<Category> GetWithProductsByIdAsync(int categoryId);
+    }
+}
