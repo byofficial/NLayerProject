17356b86035f5451b07df895c20910b5f4521909
Burak <burakylddzz@gmail.com>
2021-10-29 19:41:58 +0300

    Person Modelinin Eklenmesi ve Configuration Ayarları -Migration İşlemleri

---
+++ NLayerProject.Data/Configurations/PersonConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayerProject.Core.Models;
+
+namespace NLayerProject.Data.Configurations
+{
+    public class PersonConfiguration: IEntityTypeConfiguration<Person>
+    {
+        public void Configure(EntityTypeBuilder<Person> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn();
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(100);
+            builder.Property(x => x.SurName).IsRequired().HasMaxLength(200);
+            builder.ToTable("Persons");
+        }
+    }
+}
---
+++ NLayerProject.Data/Seeds/PersonSeed.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayerProject.Core.Models;
+
+namespace NLayerProject.Data.Seeds
+{
+    public class PersonSeed: IEntityTypeConfiguration<Person>
+    {
+   
+        public void Configure(EntityTypeBuilder<Person> builder)
+        {
+            builder.HasData(
+                new Person {Id = 1, Name = "Burak", SurName = "YILDIZ"}
+            );
+        }
+    }
+}
