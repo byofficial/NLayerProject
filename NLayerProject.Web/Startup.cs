4dfdd212e3347e45cccc53889dd73674c9e9e95e
Burak <burakylddzz@gmail.com>
2021-10-29 21:59:29 +0300

    AutoMapper Kütüphanesinin Eklenmesi ve DTO Sınıfılarının Oluşturulması

--- NLayerProject.Web/Startup.cs
+++ NLayerProject.Web/Startup.cs
@@ -31,6 +31,9 @@ namespace NLayerProject.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Nesnelerin DTO olarak dönüþtürülmesi
+            services.AddAutoMapper(typeof(Startup));
+
             services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
             services.AddScoped(typeof(IService<>), typeof(Service.Services.Service<>));
 
