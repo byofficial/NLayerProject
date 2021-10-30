7e5174f9ea05ffbe8f9fedba456519053777a712
Burak <burakylddzz@gmail.com>
2021-10-30 04:27:14 +0300

    Update() Metodunun Eklenmesi

--- NLayerProject.AWeb/Controllers/CategoriesController.cs
+++ NLayerProject.AWeb/Controllers/CategoriesController.cs
@@ -56,9 +56,9 @@ namespace NLayerProject.AWeb.Controllers
         }
 
         [HttpPost]
-        public IActionResult Update(CategoryDto categoryDto)
+        public async Task<IActionResult> Update(CategoryDto categoryDto)
         {
-            _categoryService.Update(_mapper.Map<Category>(categoryDto));
+            await _categoryApiService.Update(categoryDto);
             return RedirectToAction("Index");
         }
 
